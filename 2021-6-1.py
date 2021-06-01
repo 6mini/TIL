1a9a4365ed141b2866616c17e436d28b6cad7c1e
real6mini <real6mini@gmail.com>
2021-06-01 14:51:53 +0900

    210601

--- 2021-6-1.py
+++ 2021-6-1.py
@@ -1,4 +1,3 @@
-'''
 #클래스 : 파이선 코딩에서 중요함
 name = '마린'
 hp = 40
@@ -17,7 +16,7 @@ def attack(name, lacation, damage):
 
 attack(name, '1시', damage)
 attack(t_name, '1시', t_damage)
-'''
+
 
 '''
 marine1 = Unit('마린', 40, 5) #유닛의 인스턴스 객체
@@ -30,8 +29,8 @@ print(f'{wraith1.name},{wraith1.hp}') #외부에서도 사용가능함.
 wraith1.cloking = True
 if wraith1 == True:
     print('안녕') #클래스 외부에서 변수를 확장할 수도 있음. 확장을 한 변수에만 할당이 됨.
-
 '''
+
 class Unit: #일반유닛
     def __init__(self, name, hp, speed): # init : 생성자 셀프를 제외하고 값을 던져줘야함.
         self.name = name # 멤버변수
@@ -72,10 +71,13 @@ class FlyableAttackUnit(AttackUnit, Flyable): #다중상속받음
         AttackUnit.__init__(self, name, hp, 0, damage)
         Flyable.__init__(self, fly_speed)
 
-    def move(self, location):
+    def move(self, location): #지상유닛에 무브가 쓰였지만 재정의를 하면 여기 안에서 상속받은 함수만 사용됨.
         print('[공중 유닛 이동]')
         print(f'{self.name} : {location}으로 이동합니다. 속도 : {self.fly_speed}')
-        
+
+class BuildingUnit(Unit):
+    def __init__(self, name, hp, speed):
+        super().__init__(name, hp, speed)
 
 vul = AttackUnit('벌쳐', 80, 10, 20)
 bc = FlyableAttackUnit('배틀크루저', 500, 25, 3)
@@ -86,13 +88,13 @@ bc.fly(bc.name, '11시')
 bc.move('9시')
 
 
-'''
+
 firebat1 = AttackUnit('파이어뱃', 50, 16)
 firebat1.attack('5시')
 
 firebat1.damaged(25)
 firebat1.damaged(25)
-'''
+
 
 
 #pass : 함수에서 그냥 넘어갈수있음. 완성할수있음.

77da35c6c3f0427de4008f89dff16050f46d09bb
real6mini <real6mini@gmail.com>
2021-06-19 16:01:07 +0900

    210619

--- 2021-6-19-3.py
+++ 2021-6-19-3.py
@@ -1,3 +1,4 @@
+'''
 #pip install
 #pypi
 #pip install beautifulsoup4
@@ -5,3 +6,9 @@
 from bs4 import BeautifulSoup
 soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
 print(soup.prettify())
+'''
+
+# 파이썬에서 제공되는 내장함수, 외장함수가 있음.
+
+import glob
+print(glob.glob('*.py'))

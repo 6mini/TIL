0c85ce616fe61e8592b6feebbc5c4daa124b75c0
6mini <79494088+6mini@users.noreply.github.com>
2021-06-21 23:22:09 +0900

    musinsa snap crawling

--- google.py
+++ google.py
@@ -1,7 +1,14 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://magazine.musinsa.com/index.php?m=street&_y=2021&_mon=06%2C05%2C04&style=001')
-driver.find_elements_by_css_selector('.articleImg')[0].click() # 이미지 접속
-url = driver.find_elements_by_css_selector('.lbox')[0].get_attribute('href') # 이미지 URL 따기
-driver.get(url) # url 접속을 통한 다운로드
+PageNum = 1 # 시작 페이지
+while PageNum <= 10: # 페이지 넘버를 사용해 자동적으로 페이지가 이동되게 while문 사용
+    MuPage = f'https://magazine.musinsa.com/index.php?m=street&ordw=hit&style=011&_y=2021%2C2020&_mon=&p={PageNum}#listStart'
+    driver.get(MuPage)
+    ImgNum = 0
+    while ImgNum < 60:
+        driver.find_elements_by_css_selector('.articleImg')[ImgNum].click() # 이미지 접속
+        driver.get(driver.find_elements_by_css_selector('.lbox')[0].get_attribute('href')) # url 딴 후 접속을 통한 다운로드
+        driver.get(MuPage)
+        ImgNum += 1
+    PageNum += 1
